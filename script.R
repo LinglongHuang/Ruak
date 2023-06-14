ac3dd20f5a419771b84cca2027256e70eb0804db
Linglong Huang <linglong.huang@foxmail.com>
2023-06-14 15:17:55 +0200

    first try of pull and push

---
+++ script.R
@@ -0,0 +1,14 @@
+
+install.packages("usethis")
+install.packages("git")
+library(usethis)
+library(git)
+
+
+::git_sitrep()
+usethis::git_sitrep()
+usethis::use_git()
+usethis::use_github()
+usethis::gitadd()
+git add
+install.packages("usethis")
